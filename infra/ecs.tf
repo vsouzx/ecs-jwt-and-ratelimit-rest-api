7d1b75c7cd1d1028c3cfb8fe5a30c4ce1783ed2c
vsouzx_ <vtsoliveira2001@gmail.com>
2025-11-01 01:56:43 -0300

    a

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -28,10 +28,20 @@ resource "aws_ecs_cluster" "gofiber-api-cluster" {
 }
 
 locals {
-  container_env = [for k, v in var.container_env : {
-    name  = k
-    value = v
-  }]
+  container_env = {
+    DB_USER          = aws_db_instance.default.username
+    DB_PASSWORD      = local.db_secret.db_password
+    DB_HOST          = aws_db_instance.default.address
+    DB_PORT          = tostring(aws_db_instance.default.port)
+    DB_NAME          = aws_db_instance.default.db_name
+    JWT_SECRET       = "E-zzg4VU#pOHu=qr5rEp&PEBq86Ak#q4UQswhRnV-L6zFsdfSsFnXD-86uQ6maFdAOUpjXzAzznc#KR63mQrB&SDR2QwEB5kRjAhvHhkR3PB88NKZpHh8#D3=eufOjR99KoqxNvhfPst=QPkxR1LoenuuEsE4tQDL70fQEDhv81Kxlh4hpBvpPM7lgxIyEEejVP9=iBbcBnPS=Cll=xAumnh&kOiD3oZ=wM6Qq2xPC#6rJUf5nqUmyYBzakPTpK#27eezrXy6EMOnJOJbau4uwZ9ohuCuWGwmJrx-d6O8snAo3Wq3wlmxwIBAxPkBMkNZJq81f#U0ZerHw-JQxUYWcSq"
+    REDIS_ENDPOINT   = aws_elasticache_replication_group.redis.primary_endpoint_address
+    REDIS_PORT       = tostring(aws_elasticache_replication_group.redis.port)
+    REDIS_PASS       = "admin123@"
+    REDIS_DB         = "0"
+    RATE_LIMIT_COUNT = "10"
+    RATE_LIMIT_TTL   = "1"
+  }
 }
 
 resource "aws_ecs_task_definition" "task_definition" {
--- infra/envs/prod/terraform.tfvars
+++ infra/envs/prod/terraform.tfvars
@@ -5,7 +5,7 @@ container_env = {
   DB_PORT          = tostring(aws_db_instance.default.port)
   DB_NAME          = aws_db_instance.default.db_name
   JWT_SECRET       = "E-zzg4VU#pOHu=qr5rEp&PEBq86Ak#q4UQswhRnV-L6zFsdfSsFnXD-86uQ6maFdAOUpjXzAzznc#KR63mQrB&SDR2QwEB5kRjAhvHhkR3PB88NKZpHh8#D3=eufOjR99KoqxNvhfPst=QPkxR1LoenuuEsE4tQDL70fQEDhv81Kxlh4hpBvpPM7lgxIyEEejVP9=iBbcBnPS=Cll=xAumnh&kOiD3oZ=wM6Qq2xPC#6rJUf5nqUmyYBzakPTpK#27eezrXy6EMOnJOJbau4uwZ9ohuCuWGwmJrx-d6O8snAo3Wq3wlmxwIBAxPkBMkNZJq81f#U0ZerHw-JQxUYWcSq"
-  REDIS_ENDPOINT      = aws_elasticache_replication_group.redis.primary_endpoint_address
+  REDIS_ENDPOINT   = aws_elasticache_replication_group.redis.primary_endpoint_address
   REDIS_PORT       = tostring(aws_elasticache_replication_group.redis.port)
   REDIS_PASS       = "admin123@"
   REDIS_DB         = "0"
